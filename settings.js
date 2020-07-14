809935653fa952680c65688030a7aae30ca46a59
Saurabh Datta <hi@dattasaurabh.com>
2020-07-14 15:41:07 +0800

    modified settings for hash

--- settings.js
+++ settings.js
@@ -117,14 +117,14 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    // adminAuth: {
-    //    type: "credentials",
-    //    users: [{
-    //        username: "admin",
-    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
-    //        permissions: "*"
-    //    }]
-    // },
+     adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2a$08$2EElzUdmD9XAyeWzZBKjCumJRfJl1Mi2wKyajzWwrR4koEvyqe6BK",
+            permissions: "*"
+        }]
+     },
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
